0617cb3b80f7a7b3073fefa8ddd873bbb9fdecab
Neriya Malachi <neriamalachi@gmail.com>
2024-03-03 14:56:35 +0200

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+    basePath: isProd ? '/portfolioLinks' : '',
     output: 'export',
+    distDir: 'dist',
     images: {
         unoptimized: true
     }
