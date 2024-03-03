ee75c6ff306e1f3c79f11918eeea996b97f98bf9
Neriya Malachi <neriamalachi@gmail.com>
2024-03-03 15:14:22 +0200

    commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"BsbX2nnA9GiCz009pwfwkSJftP79DU8Hic4O3UuH4oI=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ffCzWzkhwreWunN9+anoyaunFzil5xhDo/AVsf+++oQ=\"}"
